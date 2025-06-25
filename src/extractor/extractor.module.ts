df3cabcce7ba3647b6ce94ac07da8f7e56c9dc33
Hector Zea <hectorzea@Mac.fritz.box>
2025-06-25 22:16:53 +0200

    updated projec

--- src/extractor/extractor.module.ts
+++ src/extractor/extractor.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ExtractorService } from "./extractor.service";
-import { ExtractorController } from "./extractor.controller";
 
 @Module({
   providers: [ExtractorService],
-  controllers: [ExtractorController],
   exports: [ExtractorService]
 })
 export class ExtractorModule {}
