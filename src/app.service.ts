607ba6c73c6234e4829272871067cca0941b3cff
Hector Zea <hectorzea@Mac.fritz.box>
2025-07-20 14:46:56 +0200

    updated test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,17 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `
-      <!DOCTYPE html>
-      <html style=background-color:#1e293b>
-      <head>
-        <title></title>
-      </head>
-      <body>
-        <h1>HZ Api</h1>
-        <p>Where all the data is handled...</p>
-      </body>
-      </html>
-    `;
+    return "Hello World!";
   }
 }
