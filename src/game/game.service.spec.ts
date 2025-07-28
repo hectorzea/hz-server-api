760af2a7445617e7d64368325b667124e8426d8e
Hector Zea <hectorzea@Mac.fritz.box>
2025-07-28 20:46:16 +0200

    tests

--- src/game/game.service.spec.ts
+++ src/game/game.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { GameService } from "./game.service";
+import { getModelToken } from "@nestjs/mongoose";
+import { Game } from "./schemas/game.schema";
 
 describe("GameService", () => {
   let service: GameService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GameService]
+      providers: [
+        GameService,
+        {
+          provide: getModelToken(Game.name),
+          useValue: {
+            getMulliganWinratesForAllCards: jest.fn(),
+            create: jest.fn()
+          }
+        }
+      ]
     }).compile();
 
     service = module.get<GameService>(GameService);
