9d0a13ae685778ec35447d547236100dbd29b08c
Hector Zea <hectorzea@Hectors-MacBook-Pro.local>
2025-11-04 10:26:17 +0100

    without mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,22 @@ import { MonitoringModule } from "./monitoring/monitoring.module";
 import { ExtractorModule } from "./extractor/extractor.module";
 // import { AiModule } from "./ai/ai.module";
 import { ConfigModule } from "@nestjs/config";
-import { TasksModule } from "./tasks/tasks.module";
-import { HearthstoneModule } from "./hearthstone/hearthstone.module";
-import { HearthstoneApiModule } from "./external-api/hearthstone.module";
-import { MongooseModule } from "@nestjs/mongoose";
+// import { TasksModule } from "./tasks/tasks.module";
+// import { HearthstoneModule } from "./hearthstone/hearthstone.module";
+// import { HearthstoneApiModule } from "./external-api/hearthstone.module";
+// import { MongooseModule } from "@nestjs/mongoose";
 // import { GameModule } from "./game/game.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
+    // MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
     MonitoringModule,
-    ExtractorModule,
+    ExtractorModule
     // AiModule
-    TasksModule,
-    HearthstoneModule,
-    HearthstoneApiModule
+    // TasksModule,
+    // HearthstoneModule
+    // HearthstoneApiModule
     // GameModule
   ],
   controllers: [AppController],
