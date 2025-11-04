f2fa323272bc987630ab9f14a73b2cd6e6529df1
Hector Zea <hectorzea@Hectors-MacBook-Pro.local>
2025-11-04 10:38:25 +0100

    first check mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,20 @@ import { MonitoringModule } from "./monitoring/monitoring.module";
 import { ExtractorModule } from "./extractor/extractor.module";
 // import { AiModule } from "./ai/ai.module";
 import { ConfigModule } from "@nestjs/config";
-// import { TasksModule } from "./tasks/tasks.module";
+import { TasksModule } from "./tasks/tasks.module";
 // import { HearthstoneModule } from "./hearthstone/hearthstone.module";
 // import { HearthstoneApiModule } from "./external-api/hearthstone.module";
-// import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule } from "@nestjs/mongoose";
 // import { GameModule } from "./game/game.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
+    MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
     MonitoringModule,
-    ExtractorModule
+    ExtractorModule,
     // AiModule
-    // TasksModule,
+    TasksModule
     // HearthstoneModule
     // HearthstoneApiModule
     // GameModule
