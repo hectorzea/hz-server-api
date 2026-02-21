16ad5a08f2e7b84f07af77e3fc8e4e601e9b4c68
Hector Zea <hectorzea@Hectors-MacBook-Pro.local>
2026-02-20 21:08:24 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,11 @@ async function bootstrap() {
   //   process.exit(1);
   // });
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT ?? 3001;
+
+  await app.listen(port, "0.0.0.0");
+
+  // await app.listen(process.env.PORT ?? 3001);
 }
 
 bootstrap()
