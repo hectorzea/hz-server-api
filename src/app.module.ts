b56b94dc9c8d179f730eb8c1832450b94bdd15c5
Hector Zea <hectorzea@Hectors-MacBook-Pro.local>
2026-02-20 21:52:28 -0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,11 @@ import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
-    AiModule,
-    TasksModule,
-    HearthstoneModule
+    ConfigModule.forRoot()
+    // MongooseModule.forRoot(process.env.MONGO_DB_CLUSTER_URL!),
+    // AiModule,
+    // TasksModule,
+    // HearthstoneModule
     // HearthstoneApiModule
     // GameModule
     // MonitoringModule,
